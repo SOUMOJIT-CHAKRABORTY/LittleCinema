376eaa5b5b3ad8ab45c0520a372649ff7c019e84
SOUMOJIT-CHAKRABORTY <goutamghambir2018@gmail.com>
2023-07-19 00:08:50 +0530

    now able to take image

--- sanity/schemas/page-schema.ts
+++ sanity/schemas/page-schema.ts
@@ -18,7 +18,40 @@ const page = {
       name: "content",
       title: "Content",
       type: "array",
-      of: [{ type: "block" }],
+      of: [
+        {
+          type: "block",
+          styles: [
+            { title: "Normal", value: "normal" },
+            { title: "H1", value: "h1" },
+            { title: "H2", value: "h2" },
+            { title: "H3", value: "h3" },
+            { title: "Quote", value: "blockquote" },
+          ],
+          marks: {
+            decorators: [{ title: "Strong", value: "strong" }],
+            annotations: [
+              {
+                name: "image",
+                type: "image",
+                title: "Image",
+                fields: [
+                  {
+                    name: "caption",
+                    type: "string",
+                    title: "Caption",
+                  },
+                  {
+                    name: "alt",
+                    type: "string",
+                    title: "Alt Text",
+                  },
+                ],
+              },
+            ],
+          },
+        },
+      ],
     },
   ],
 };
--- sanity/schemas/project-schema.ts
+++ sanity/schemas/project-schema.ts
@@ -36,7 +36,40 @@ const project = {
       name: "content",
       title: "Content",
       type: "array",
-      of: [{ type: "block" }],
+      of: [
+        {
+          type: "block",
+          styles: [
+            { title: "Normal", value: "normal" },
+            { title: "H1", value: "h1" },
+            { title: "H2", value: "h2" },
+            { title: "H3", value: "h3" },
+            { title: "Quote", value: "blockquote" },
+          ],
+          marks: {
+            decorators: [{ title: "Strong", value: "strong" }],
+            annotations: [
+              {
+                name: "image",
+                type: "image",
+                title: "Image",
+                fields: [
+                  {
+                    name: "caption",
+                    type: "string",
+                    title: "Caption",
+                  },
+                  {
+                    name: "alt",
+                    type: "string",
+                    title: "Alt Text",
+                  },
+                ],
+              },
+            ],
+          },
+        },
+      ],
     },
   ],
 };
